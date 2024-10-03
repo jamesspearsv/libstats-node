510047c8e6b3d56c3137611e4b0345889afe5292
James Spears <jamesspearsv@gmail.com>
2024-10-03 17:24:14 -0400

    Update SelectInput.jsx

--- client/src/components/SelectInput.jsx
+++ client/src/components/SelectInput.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import styles from './SelectInput.module.css';
 
-function SelectInput({ title, values, handleChange, formState }) {
+function SelectInput({ title, name, values, handleChange, formState }) {
   const style = {
     display: 'flex',
     flexDirection: 'column',
@@ -13,10 +13,10 @@ function SelectInput({ title, values, handleChange, formState }) {
     <div style={style} className={styles.customSelect}>
       <label htmlFor={id}>{title}</label>
       <select name={id} id={id} onChange={handleChange} value={formState}>
-        <option value="" disabled>{`Select ${title}`}</option>
+        <option value='' disabled>{`Select ${title}`}</option>
         {values.map((value, index) => (
-          <option key={index} value={value.toLowerCase()}>
-            {value}
+          <option key={index} value={value}>
+            {name}
           </option>
         ))}
       </select>
@@ -26,6 +26,7 @@ function SelectInput({ title, values, handleChange, formState }) {
 
 SelectInput.propTypes = {
   title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   values: PropTypes.array.isRequired,
   handleChange: PropTypes.func.isRequired,
   formState: PropTypes.string.isRequired,
