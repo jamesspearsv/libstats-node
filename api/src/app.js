e759f15dac8a4321b6195f58f18605e1fc2968a0
James Spears <jamesspearsv@gmail.com>
2024-10-02 21:02:36 -0400

    Set up Knex for query building

--- api/src/app.js
+++ api/src/app.js
@@ -1,23 +1,43 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3002;
-const MODE = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const config = require('../knexfile');
+const knex = require('knex');
+const db = knex(config[NODE_ENV]);
+
 app.use((req, res, next) => {
-  console.log(req.body);
-  console.log(`${new Date()}: ${req.body}`);
+  console.log(`${new Date()} -- URL: ${req.url}`);
   next();
 });
 
-app.get('/', (req, res) => {});
+app.get('/', async (req, res) => {
+  const rows = await db
+    .select(
+      'interactions.interaction_id',
+      'types.type_name',
+      'locations.location_name'
+    )
+    .from('interactions')
+    .join('types', 'interactions.type_id', '=', 'types.type_id')
+    .join(
+      'locations',
+      'interactions.location_id',
+      '=',
+      'locations.location_id'
+    );
+  res.json(rows);
+});
 
 app.listen(PORT, () => {
   console.log('##########################');
-  console.log(`- Running in ${MODE} mode`);
+  console.log(`- Running in ${NODE_ENV} mode`);
   console.log(`- Server listening on port ${PORT}`);
   console.log('##########################');
 });
--- api/src/db/migrations/20241003005350_create_db.js
+++ api/src/db/migrations/20241003005350_create_db.js
@@ -15,7 +15,7 @@ exports.up = function (knex) {
       table.string('location_label').notNullable();
     })
     .createTable('interactions', (table) => {
-      table.increments('interation_id').notNullable();
+      table.increments('interaction_id').notNullable();
       table.integer('type_id').notNullable();
       table.integer('location_id').notNullable();
       table.foreign('type_id').references('types.type_id');
