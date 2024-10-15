448cde6d842ed2d3f6fb69857d3d8088473bdd2e
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2024-10-15 16:46:27 -0400

    Update database types seed file

--- api/src/db/seeds/02_populate_types.js
+++ api/src/db/seeds/02_populate_types.js
@@ -6,10 +6,30 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('types').del();
   await knex('types').insert([
-    { id: 1, value: 'Directional', desc: '' },
-    { id: 2, value: 'Digital Resources', desc: '' },
-    { id: 3, value: 'Known-Item Requests', desc: '' },
-    { id: 4, value: 'Information Services', desc: '' },
-    { id: 5, value: 'Tech Help', desc: '' },
+    {
+      id: 1,
+      value: 'Directional',
+      desc: 'Simple questions about facilities, hours, etc.',
+    },
+    {
+      id: 2,
+      value: 'Digital Resources',
+      desc: 'Questions about accessing and using MPL digital resources.',
+    },
+    {
+      id: 3,
+      value: 'Known-Item Requests',
+      desc: 'Patron questions about specific items (known-items).',
+    },
+    {
+      id: 4,
+      value: 'Information Services',
+      desc: 'General reference questions and questions about library cards or services.',
+    },
+    {
+      id: 5,
+      value: 'Tech Help',
+      desc: 'Requests for help using public computers or other tech questions',
+    },
   ]);
 };
