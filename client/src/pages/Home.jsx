8af83a71c6f91a2658931bf4b4325b3b2a056af4
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2024-10-22 17:09:15 -0400

    Build out home page UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, Link } from 'react-router-dom';
 import CardWrapper from '../components/CardWrapper';
 import Button from '../components/Button';
-import SlotCounter from 'react-slot-counter';
+import { toast } from 'react-hot-toast';
 
 import styles from './Home.module.css';
 
 function Home() {
   // eslint-disable-next-line no-unused-vars
   const [apiurl, setApiurl] = useOutletContext();
+  const [types, setTypes] = useState([]);
   const [data, setData] = useState({});
   const [effectTrigger, setEffectTrigger] = useState(true);
   const [loading, setLoading] = useState(true);
@@ -19,10 +20,17 @@ function Home() {
 
       const data = await fetch(url);
       const json = await data.json();
+      setData(json);
 
       console.log(json);
 
-      setData(json);
+      const options = `${apiurl}/options`;
+      const optionsData = await fetch(options);
+      const optionsJson = await optionsData.json();
+      setTypes(optionsJson.types);
+
+      console.log(optionsJson.types);
+
       setLoading(false);
     }
 
@@ -32,32 +40,50 @@ function Home() {
   function handleRefresh() {
     setLoading(true);
     setEffectTrigger((effectTrigger) => !effectTrigger);
+    toast.loading('Refreshing', {
+      duration: 1000,
+    });
   }
 
   return (
     <>
       <h1 className={styles.heading}>LibStats</h1>
-      <hr />
-      <CardWrapper>
-        <div className={styles.monthly}>
-          <div className={styles.monthly_label}>Interactions This Month</div>
-          <div style={{ minHeight: '25px' }}>
-            {!loading && (
-              <SlotCounter
-                startValue={'0'}
-                value={data.count_month}
-                useMonospaceWidth
-              />
-            )}
-          </div>
-          <Button
-            text="Refresh"
-            variant="primary"
-            action={handleRefresh}
-            style={{ paddingLeft: '2rem', paddingRight: '2rem' }}
-          />
+      <hr className={styles.divider} />
+      <div className={styles.container}>
+        <div className={styles.quickstart}>
+          <p>
+            LibStats is a simple app that allows you to record, and build
+            reports reference interactions that are completed in the library.
+            Click <Link to={'/record'}>Record</Link> in the menu to get started.
+          </p>
         </div>
-      </CardWrapper>
+        <CardWrapper>
+          <div className={styles.monthly}>
+            <div className={styles.monthly_label}>Interactions This Month</div>
+            <div style={{ minHeight: '25px' }}>
+              <div>{data.count_month}</div>
+            </div>
+            <Button
+              text="Refresh"
+              variant="primary"
+              action={handleRefresh}
+              style={{ paddingLeft: '2rem', paddingRight: '2rem' }}
+            />
+          </div>
+        </CardWrapper>
+      </div>
+      <div className={styles.types}>
+        {types.map((type) => (
+          <CardWrapper
+            key={type.id}
+            style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+          >
+            <div>{type.value}</div>
+            <hr />
+            <div>{type.desc}</div>
+          </CardWrapper>
+        ))}
+      </div>
     </>
   );
 }
