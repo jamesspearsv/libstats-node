dc144553e2fa49f410e16e39796187498a59b2b7
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2024-10-25 14:35:40 -0400

    Code clean up

--- api/src/app.js
+++ api/src/app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./routes/router');
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/router");
 
 const app = express();
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || "development";
 const PORT = process.env.NODE_PORT || 3001;
 
 const origins = process.env.ORIGINS
-  ? process.env.ORIGINS.split(',')
-  : ['http://localhost:3000'];
+  ? process.env.ORIGINS.split(",")
+  : ["http://localhost:3000"];
 
 app.use(
   cors({
     origin: origins,
-    methods: ['GET', 'POST'],
+    methods: ["GET", "POST"],
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,8 +31,8 @@ app.use((req, res, next) => {
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log('##########################');
+  console.log("##########################");
   console.log(`- Running in ${NODE_ENV} mode`);
   console.log(`- Server listening on port ${PORT}`);
-  console.log('##########################');
+  console.log("##########################");
 });
