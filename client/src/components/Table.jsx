50a336eab8cff4c58ff512133e2eab44b777e1f1
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2024-10-30 14:38:31 -0400

    Rebuild custom table component to improve styling ability

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -2,36 +2,28 @@ import PropTypes from "prop-types";
 import styles from "./Table.module.css";
 
 /*
- * Component to parse and render a table of interactions:
- *     Rows prop is required and expected to be an array of objects representing interactions.
- *     Each row in rows should contain the following properties: id, type, format, locations, and date
+ * Component to parse and render a table of interactions
  */
 
 function Table({ rows }) {
   return (
     <div className={styles.table}>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Type</th>
-            <th>Format</th>
-            <th>Location</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, index) => (
-            <tr key={index}>
-              <td>{row.id}</td>
-              <td>{row.type}</td>
-              <td>{row.format}</td>
-              <td>{row.location}</td>
-              <td>{row.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className={styles.row}>
+        <div className={styles.cell}>ID</div>
+        <div className={styles.cell}>Type</div>
+        <div className={styles.cell}>Format</div>
+        <div className={styles.cell}>Location</div>
+        <div className={styles.cell}>Date</div>
+      </div>
+      {rows.map((row, index) => (
+        <div key={index} className={styles.row}>
+          <div className={styles.cell}>{row.id}</div>
+          <div className={styles.cell}>{row.type}</div>
+          <div className={styles.cell}>{row.format}</div>
+          <div className={styles.cell}>{row.location}</div>
+          <div className={styles.cell}>{row.date}</div>
+        </div>
+      ))}
     </div>
   );
 }
