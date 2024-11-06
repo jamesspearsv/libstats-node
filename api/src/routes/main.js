b44f7421654c8b28f985d6c5caa8783357f516d3
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2024-11-06 15:51:02 -0500

    Remove index route from routes/main.js
    Signed-off-by: jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>

--- api/src/routes/main.js
+++ api/src/routes/main.js
@@ -4,9 +4,6 @@ const appController = require("../controllers/main");
 const router = Router();
 
 // ** ROUTES ** //
-router.get("/", (req, res) => {
-  res.status(403).send("<h1>403 Forbidden</h1>");
-});
 router.get("/interactions", appController.interactionsGet);
 router.get("/options", appController.optionsGet);
 router.post("/record", appController.recordPost);
